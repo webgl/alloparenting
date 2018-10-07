90c3017e93f53c1d1be265bc937f43048655dacc
Daniel <dmockaitis@att.net>
2018-10-06 23:15:02 -0700

    HEUPBEPIUBPIUEB

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,8 +7,8 @@ function Card(props) {
   const { name, picture, gender, location } = props;
   return (
     <div className="profile">
+      <img src={`https://picsum.photos/200/300/?random/${Math.ceil(Math.random() * Math.floor(10000))}`} className="background"/>
       <div className="top-part">
-        <img src={`https://picsum.photos/200/300/?random/${Math.ceil(Math.random() * Math.floor(10000))}`} className="background"/>
         <img src={picture} className="picture"/>
       </div>
     </div>
