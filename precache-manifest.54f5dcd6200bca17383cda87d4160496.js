71c6c5e0b7ace14bf53a6a9bf3e9463c94bb06a8
Farhad Ghayour <me@farhadg.com>
2018-10-07 07:28:53 -0700

    Updates

---
+++ precache-manifest.54f5dcd6200bca17383cda87d4160496.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b91fc1aa49491bf18a729952868615c",
+    "url": "/nurture/static/media/parents.4b91fc1a.jpg"
+  },
+  {
+    "revision": "9f91568599bcf45b7396",
+    "url": "/nurture/static/css/main.2714f651.chunk.css"
+  },
+  {
+    "revision": "10899d756f8de3b80276",
+    "url": "/nurture/static/js/1.10899d75.chunk.js"
+  },
+  {
+    "revision": "66c74483bf91048ab7ab",
+    "url": "/nurture/static/js/runtime~main.66c74483.js"
+  },
+  {
+    "revision": "3ca0d179e920d0119452ef7ffc82726c",
+    "url": "/nurture/static/media/logo.3ca0d179.png"
+  },
+  {
+    "revision": "6a29f8f4b8390c0d4f8eab05fa8b72c2",
+    "url": "/nurture/static/media/family.6a29f8f4.jpg"
+  },
+  {
+    "revision": "9f91568599bcf45b7396",
+    "url": "/nurture/static/js/main.9f915685.chunk.js"
+  },
+  {
+    "revision": "0cc36bfac86b8104779911c77002e5a6",
+    "url": "/nurture/static/media/cryingbabyonplane.0cc36bfa.gif"
+  },
+  {
+    "revision": "ebbdec1d2fccf423281273e325d69655",
+    "url": "/nurture/static/media/fisherman.ebbdec1d.jpg"
+  },
+  {
+    "revision": "6c5121c393f46ef68565985476814aad",
+    "url": "/nurture/static/media/halloween.6c5121c3.jpg"
+  },
+  {
+    "revision": "2ee80f96fedc622bd3a0ab61486b5112",
+    "url": "/nurture/static/media/vegas.2ee80f96.jpg"
+  },
+  {
+    "revision": "93a97198d42c1bc3cd669b8f0c622ea3",
+    "url": "/nurture/index.html"
+  }
+];
