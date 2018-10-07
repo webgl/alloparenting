39cfc69f3e1ff380048b964a00897733ba07e41d
Daniel <dmockaitis@att.net>
2018-10-06 23:55:16 -0700

    DONE

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-
-import { getRandomAction } from '../../helpers';
+import faker from 'faker';
+import { getRandomAction, capitalize } from '../../helpers';
 
 import './Card.css'
 
@@ -11,6 +11,7 @@ function Card(props) {
         <img src={`https://picsum.photos/200/300/?random/${Math.ceil(Math.random() * Math.floor(10000))}`} className="background"/>
         <div className="info">
           <img src={picture} className="picture"/>
+          <h4>{capitalize(name)} - {faker.random.number(10)} miles away</h4>
           <h1>{getRandomAction()}</h1>
         </div>
     </div>
