aee1f2d1314351c962ad927ab0586ef5ac987e4d
MirasServiceStork <mirasservicestork@gmail.com>
2018-10-06 19:18:33 -0700

    Working on the card.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,14 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Grid>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-        </Grid>
+        <Card/>
       </div>
     );
   }
