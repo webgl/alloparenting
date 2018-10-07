bfa3a470978a5df4b7d7955205270c8ccbb527d8
Daniel <dmockaitis@att.net>
2018-10-06 23:45:12 -0700

    HEPWJEOPFN

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import faker from 'faker';
+
+import { getRandomAction } from '../../helpers';
 
 import './Card.css'
 
@@ -7,10 +8,11 @@ function Card(props) {
   const { name, picture, gender, location } = props;
   return (
     <div className="profile">
-      <img src={`https://picsum.photos/200/300/?random/${Math.ceil(Math.random() * Math.floor(10000))}`} className="background"/>
-      <div className="top-part">
-        <img src={picture} className="picture"/>
-      </div>
+        <img src={`https://picsum.photos/200/300/?random/${Math.ceil(Math.random() * Math.floor(10000))}`} className="background"/>
+        <div className="info">
+          <img src={picture} className="picture"/>
+          <h1>{getRandomAction()}</h1>
+        </div>
     </div>
   )
 }
--- src/helpers/form-helpers.js
+++ src/helpers/form-helpers.js
@@ -6,3 +6,35 @@ export function handleInputChange(event) {
       [name]: value
     });
 }
+
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const ACTIONS = [
+  'walk the dog',
+  'drink wine',
+  'learn philosphy',
+  'traditional Ethiopian meal',
+  'camping',
+  'hiking yosemite',
+  'math tutor',
+  'pickup from Yosemite High',
+  '3:00PM soccer practice',
+  'traditional lithuanian dance',
+  'nerf gun battle',
+  'skip rocks on the lake',
+  'listen to live music',
+  'attend my family piano recital',
+  'learn spanish in 2 hours',
+  'help around the garden',
+  'make a traditional Swahili dress',
+  'have a chess match',
+  'make Italian Pasta'
+];
+
+export function getRandomAction() {
+    let num = Math.floor(Math.random() * Math.floor(ACTIONS.length));
+    return capitalize(ACTIONS[num])
+}
